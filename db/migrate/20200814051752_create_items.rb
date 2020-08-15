a294b6cae2ffaf37cdd2c2b514cd4c82b7fdfd87
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-15 09:32:25 +0900

    Rails課題Q4回答致しました。

---
+++ db/migrate/20200814051752_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      # ここから
+      t.string :name
+      t.integer :price
+      t.integer :user_id
+      # ここまで
+      t.timestamps
+    end
+  end
+end
